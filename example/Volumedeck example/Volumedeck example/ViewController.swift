58241a2b1063bc03126a945ad2e2082d5a1102d5
Foti Dim <fdimanidis@gmail.com>
2023-01-20 23:12:29 +0100

    Add mock speed UI

--- example/Volumedeck example/Volumedeck example/ViewController.swift
+++ example/Volumedeck example/Volumedeck example/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var speedSlider: UISlider!
+    @IBOutlet weak var speedLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        speedLabel.text = String(speedSlider.value)
     }
 
+   
 
+    @IBAction func speedChanged(_ sender: UISlider) {
+        let step: Float = 0.5
+        let roundedValue = round(sender.value / step) * step
+        sender.value = roundedValue
+        speedLabel.text = String(speedSlider.value)
+    }
+    
+    @IBAction func sendSpeed(_ sender: Any) {
+        (view.window?.windowScene?.delegate as? SceneDelegate)?.volumedeck?.mockSpeed(speed: Double(speedSlider.value))
+    }
 }
 
