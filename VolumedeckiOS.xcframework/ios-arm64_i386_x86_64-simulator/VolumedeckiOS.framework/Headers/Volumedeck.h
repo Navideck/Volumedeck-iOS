b4c45cc3d6ea523a09f1024a0dad374964b9209f
Foti Dim <fdimanidis@gmail.com>
2023-01-20 07:55:43 +0100

    Add xcframework

---
+++ VolumedeckiOS.xcframework/ios-arm64_i386_x86_64-simulator/VolumedeckiOS.framework/Headers/Volumedeck.h
@@ -0,0 +1,19 @@
+//
+//  Volumedeck.h
+//  Volumedeck
+//
+//  Created by Foti Dim on 05.08.20.
+//  Copyright © 2020 navideck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Volumedeck.
+FOUNDATION_EXPORT double VolumedeckVersionNumber;
+
+//! Project version string for Volumedeck.
+FOUNDATION_EXPORT const unsigned char VolumedeckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Volumedeck/PublicHeader.h>
+
+
